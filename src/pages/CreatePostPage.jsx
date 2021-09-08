795ac6eb1402aec616bacbc5727a42238e7a655c
gregdanko92 <gregdanko92@gmail.com>
2021-09-08 13:55:22 -0400

    create page

--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-class CreatePostPage extends React.Component{
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
-    render()
-    {
-        return(
-        <div>
-                <h1>summer note goes hurr</h1>
-        </div>
-        )
-    }
+class MyEditor extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = editorState => this.setState({editorState});
+  }
+
+    MyInput = () => {
+    const [value, setValue] = useState('');
+    const onChange = (evt) => setValue(evt.target.value);
+  
+    return <input value={value} onChange={onChange} />;
+  };
+
+  render() {
+    return (
+      <Editor editorState={this.state.editorState} onChange={this.onChange} />
+    );
+  }
 }
 
+ReactDOM.render(<MyEditor />, document.getElementById('container'));
 
-export default CreatePostPage
+export default MyEditor
